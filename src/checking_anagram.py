11a055185de70ba60444d72b41b19f9ff34bae5a
Andrey Marinov <heavenlymage97@gmail.com>
2022-09-29 15:42:48 +0300

    Program that checks if two strings are anagram

--- src/checking_anagram.py
+++ src/checking_anagram.py
@@ -1,9 +1,11 @@
 '''Modul that checks if two strings are anagram or not'''
 
 
-def is_anagram(first_word, second_word):
-    '''Check if first_word is equal to second_word and return it'''
+def is_anagram(first_word:str, second_word:str) -> bool:
+    '''Check if first_word is equal to second_word and return it
+
+    :param: first_word:    str first word to compare with the second
+    :param: second_word:   str second word to compare with the first
+    :return: bool if first word is anagram of second word.
+    '''
     return sorted(first_word) == sorted(second_word)
-
-
-is_anagram('haoya', 'ahoya')
